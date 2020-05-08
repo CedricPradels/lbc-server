7b6f460a6d103e9c02441109dcdd95e9a6ea7857
CedricPradels <cedric.pradels@gmail.com>
2020-05-08 09:34:56 +0200

    Init DB

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ import { ApolloServer } from "apollo-server";
 import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
 
+import mongoose from "mongoose";
+mongoose.connect(String(process.env.MONGODB_URI), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.once("open", () => console.log("DB connected"));
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server
