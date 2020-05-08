6f663b4a2d21a50f054c46ac946ed1f02fabbc02
CedricPradels <cedric.pradels@gmail.com>
2020-05-08 13:56:00 +0200

    Add login

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,17 +1,31 @@
 import User from "../models/User";
 
-import { createAuthentication } from "../helpers/authentication";
+import { createAuthentication, checkPassword } from "../helpers/authentication";
 import { passwordP, emailP } from "../helpers/predicates";
 
 import { IResolvers } from "apollo-server";
 
 const resolvers: IResolvers = {
+  Query: {
+    login: async (_, args) => {
+      try {
+        const { email, password } = args;
+        const queryUser = await User.findOne({ email }).lean(true);
+        if (queryUser === null) throw "User not found";
+        const { hash, salt } = queryUser;
+        if (!checkPassword({ password, hash, salt })) throw "Invalid password";
+
+        return queryUser;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   Mutation: {
-    register: async (parent, args, context, info) => {
+    register: async (_, args) => {
       try {
         const { alias = "NoName", password, email } = args;
 
-        // EMAIL ALREADY EXIST
         const queryEmail = await User.findOne({ email });
         if (queryEmail !== null) throw "Email already exist";
         if (!passwordP(password)) throw "Invalid password";
--- src/helpers/authentication.ts
+++ src/helpers/authentication.ts
@@ -3,21 +3,36 @@ import randomstring from "randomstring";
 import sha256 from "crypto-js/sha256";
 import Base64 from "crypto-js/enc-base64";
 
+type password = string;
+type salt = string;
+type hash = string;
+type token = string;
+
 const encrypt = (str: string): string => sha256(str).toString(Base64);
 const createRandomStr = (): string => randomstring.generate(64);
 const createHash = ({
   salt,
   password,
 }: {
-  salt: string;
-  password: string;
-}): string => encrypt(salt + password);
+  salt: salt;
+  password: password;
+}): hash => encrypt(salt + password);
 
 export const createAuthentication = (
-  password: string
-): { hash: string; token: string; salt: string } => {
+  password: password
+): { hash: hash; token: token; salt: salt } => {
   const token = createRandomStr();
   const salt = createRandomStr();
   const hash = createHash({ salt, password });
   return { token, salt, hash };
 };
+
+export const checkPassword = ({
+  password,
+  hash,
+  salt,
+}: {
+  password: password;
+  hash: hash;
+  salt: salt;
+}): boolean => createHash({ password, salt }) === hash;
